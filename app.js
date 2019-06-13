92634083a4d8b9c0f7811a82b03e40a68f1f11fc
Lauren Eggers <neyabenz@gmail.com>
2019-06-13 18:05:30 -0400

    packages installed & setup

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
 
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect('mongodb://localhost/blogrest', {useNewUrlParser: true});
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost/restful_blog', {useNewUrlParser: true});
 
 
 //title
@@ -15,3 +18,6 @@ mongoose.connect('mongodb://localhost/blogrest', {useNewUrlParser: true});
 //body
 
 //created
+app.listen(3000, () => {
+    console.log('Listening on PORT 3000')
+})
