b5c86d0d5c4b626a006d9a38a0c2e9484a45b9f6
Lauren Eggers <neyabenz@gmail.com>
2019-06-18 12:03:49 -0400

    new posts save to mongodb, redirect back to blogs

--- app.js
+++ app.js
@@ -39,6 +39,20 @@ app.get('/blogs/new', (req, res) => {
     res.render('new');
 });
 //create route
+app.post('/blogs', (req, res) => {
+    Blog.create({
+        title: req.body.blog.title,
+        image: req.body.blog.image,
+        body: req.body.blog.body
+    }, (err, blog) => {
+        if(err){
+            res.send('There was an error posting, go back and try again')
+        } else {
+            console.log("Added a new blog post", blog);
+        }
+    });
+    res.redirect('/blogs')
+});
 
 app.listen(3000, () => {
     console.log('Listening on PORT 3000');
